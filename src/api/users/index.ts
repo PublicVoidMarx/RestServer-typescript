5344f9a5a8a483e5bd9d05785ca9a9fccfc69e0b
luciano criscuolo <luchigroso9@hotmail.com>
2023-04-03 14:59:07 -0300

    users endpoints

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
-import { getUsers } from './controller'
+import { createUser, getUserById, getUsers } from './controller'
 
 export const router = Router()
 
 router.get('/', getUsers)
+router.get('/:id', getUserById)
+router.post('/', createUser)
