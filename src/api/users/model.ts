c0f3de4a227008eaac710d216cb247c84b740509
luciano criscuolo <luchigroso9@hotmail.com>
2023-04-03 15:10:52 -0300

    master 2

--- src/api/users/model.ts
+++ src/api/users/model.ts
@@ -21,7 +21,7 @@ const UserSchema = new Schema<Iuser>({
     lowercase: true,
     unique: true,
     required: true,
-    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email addresss']
   },
   age: {
     type: Number,
